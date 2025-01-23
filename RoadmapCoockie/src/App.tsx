79ae1ac7f3196fd0b34e51df17ae7dc774024f89
Sarfaraz Shaik <63334137+ShaikSarfaraz@users.noreply.github.com>
2025-01-23 19:31:02 +0530

    Commented in the invalid cade and made some logic changes for the cookies container

--- RoadmapCoockie/src/App.tsx
+++ RoadmapCoockie/src/App.tsx
@@ -4,8 +4,10 @@ import CookiesLogo from './assets/Cookies-Logo.jpg';
 function App() {
   return (
     <>
+    {/* This is the cookies Parent Container that i have created */}
       <div className='borderlines bg-black'>
         <h1>coockies</h1>
+        {/* This is the chailed container inside the Parent container */}
         <div className='cookiesContainer'>
           <div>
             <img className='logo' src={CookiesLogo} alt="Cookies Logo" />
